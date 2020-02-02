da8923340d44088938ca7333df3f430075e0174f
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2020-02-02 13:30:41 +0300

    Delete Course from DB.

---
+++ src/main/java/com/luv2code/DeleteCourseAndReviewsDemo.java
@@ -0,0 +1,48 @@
+package com.luv2code;
+
+import com.luv2code.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteCourseAndReviewsDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        try (factory; Session session = factory.getCurrentSession()) {
+
+            //start a transaction
+            session.beginTransaction();
+
+            //get the course
+            int theId = 10;
+            Course tempCourse = session.get(Course.class, theId);
+
+            //print the course
+            System.out.println("Deleting the course ...");
+            System.out.println(tempCourse);
+
+            //print the course reviews
+            System.out.println(tempCourse.getReviews());
+
+            //delete the course
+            session.delete(tempCourse);
+
+            //commit transaction
+            session.getTransaction().commit();
+
+
+            System.out.println("Done!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
